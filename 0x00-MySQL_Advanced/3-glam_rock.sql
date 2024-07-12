4d779dcaaa6682925bf8a5fbed1028747f94f5d2
root <root@DESKTOP-7J5J775>
2024-07-12 11:11:22 +0300

    use like

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,5 +4,5 @@
 SELECT band_name, 
         IF(split IS NULL, 2022 - formed, split - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
