b4a207d96f6542a918eb6e5887904a1671cb386e
root <root@DESKTOP-7J5J775>
2024-07-12 10:54:32 +0300

    3 glam rocks

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- 3-glam_rock.sql
 -- lists all bands with Glam rock ranked by logevity
 
-SELECT band_name, (2022 - formed) AS lifespan
+SELECT band_name, 
+        IF(split IS NULL, 2022 - formed, split - formed) AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
