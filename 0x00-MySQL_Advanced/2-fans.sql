b99eaf291d2a6bc58acf26474267df9b7d24499b
Wambuuyy <wambuiprudy@gmail.com>
2024-07-12 10:38:56 +0300

    correct the from

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- column names: origin and nb_fans
 -- script executed by any database
 
-SELECT origin, COUNT(fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
