73ae11f6f0720e376de3fbb520bd2ce7b9f9a769
root <root@DESKTOP-7J5J775>
2024-07-12 11:19:16 +0300

    correct how to get quantity

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,7 +5,8 @@ DELIMITER $$
 CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    UPDATE items SET quantity - NEW.number
+    UPDATE items
+    SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
 END$$
 DELIMITER ;
